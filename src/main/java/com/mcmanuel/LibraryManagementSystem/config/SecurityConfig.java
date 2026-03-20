620cb7afb7a2f290f99b88b9a2a7e39b11d45a6f
unknown <mcmanuel755@gmail.com>
2026-03-21 00:47:25 +0100

    configuration classes

--- src/main/java/com/mcmanuel/LibraryManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/mcmanuel/LibraryManagementSystem/config/SecurityConfig.java
@@ -1,20 +1,55 @@
 package com.mcmanuel.LibraryManagementSystem.config;
 
+import com.mcmanuel.LibraryManagementSystem.services.MyUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private MyUserDetailsService userDetailsService;
+    private JwtConfiguration jwtConfiguration;
 
-    public HttpSecurity configuration(HttpSecurity http){
+    @Bean
+    public SecurityFilterChain configuration(HttpSecurity http){
         return http
                 .authorizeHttpRequests(
-                        request -> request.requestMatchers()
+                        request -> request.requestMatchers("/api/v1/login","/api/v1/users/").permitAll().anyRequest().authenticated()
                 )
-                .cors()
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf()
-                .addFilterBefore()
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtConfiguration, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
