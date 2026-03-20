9d2cacc6ec197951282a912e6d98119d10930394
unknown <mcmanuel755@gmail.com>
2026-03-20 23:26:59 +0100

    all

---
+++ src/main/java/com/mcmanuel/LibraryManagementSystem/pojo/UserRequest.java
@@ -0,0 +1,4 @@
+package com.mcmanuel.LibraryManagementSystem.pojo;
+
+public class UserRequest {
+}
---
+++ src/main/java/com/mcmanuel/LibraryManagementSystem/services/intf/UserService.java
@@ -0,0 +1,22 @@
+package com.mcmanuel.LibraryManagementSystem.services.intf;
+
+import com.mcmanuel.LibraryManagementSystem.DTO.UserDTO;
+import com.mcmanuel.LibraryManagementSystem.entities.User;
+import com.mcmanuel.LibraryManagementSystem.pojo.UserRequest;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+
+    UserDTO createUser(UserRequest user);
+
+    UserDTO getUser(UUID userId);
+
+    List<UserDTO> getAllUser();
+
+    UserDTO updateUser(UUID userId, User user);
+
+    UserDTO deleteeUser(UUID userId);
+
+}
